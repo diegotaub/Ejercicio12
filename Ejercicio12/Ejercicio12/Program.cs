76fc4210e7cfceb24895fb58962195da5abfb85a
diego.taub <diego.taub@accenture.com>
2021-03-21 20:48:34 -0300

    Ejercicio12

---
+++ Ejercicio12/Ejercicio12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la primera frase: ");
+            string frase = Console.ReadLine();
+            Console.Write("Ingrese la segunda frase: ");
+            if (frase == Console.ReadLine()){
+                Console.Write("Las frases son iguales.");
+            }
+            else
+            {
+                Console.Write("Las frases son diferentes.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
